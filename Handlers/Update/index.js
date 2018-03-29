fba91de73c282fe1439f58f838a462c4367b9a54
Henry <henrypye@outlook.com>
2018-03-29 16:34:48 +0100

    Added RetrieveState function

--- Handlers/Update/index.js
+++ Handlers/Update/index.js
@@ -11,12 +11,14 @@ module.exports = class Update {
             if (!params.ip || !params.state)
                 return reject("Unable to update device, no device or state given");
 
-            request.post({ headers: {'content-type' : 'application/json'},
-                            url: 'http://' + params.address + ':8000/api/state/changestate', 
-                            body: JSON.stringify({state : params.state}) }, 
-                function optionalCallback(err, httpResponse, body) {
-                    if (err) 
-                        return reject("Change state failed" + err);                  
+            request.post({ 
+                headers: {'content-type' : 'application/json'},
+                url: 'http://' + params.address + ':8000/api/state/changestate', 
+                body: JSON.stringify({state : params.state}) 
+            }, 
+            function optionalCallback(err, httpResponse, body) {
+                if (err) 
+                    return reject("Change state failed" + err);                  
 
                 return resolve("Updated state");
             });
