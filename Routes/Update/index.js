c920b741a1f9b68f041f0c4572128c22a6c1d4f4
Henry <henrypye@outlook.com>
2018-03-29 16:08:04 +0100

    Completed skeletons

--- Routes/Update/index.js
+++ Routes/Update/index.js
@@ -1,7 +1,13 @@
-var route = require('express').Router();
+/* jshint esversion: 6*/
+var route = require('express').Router(),
+    Updater = new(require('../../Handlers/Update'))();
 
 route.post('/state', function(req, res) {
-
+    Updater.UpdateState(req.body).then(newState => {
+        return res.status(200).send({ "state " : + newState });
+    }).catch(error => {
+        return res.status(400).send({ "error " : error });
+    });
 });
 
 module.exports = route;
