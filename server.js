da810dca5f781b54e567de58ec9ee0251fc806ec
Henry Pye <henrypye@outlook.com>
2018-04-02 15:11:07 +0100

    Port now uses config

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     RetrieveRoutes = require('./Routes/Retrieve'),
-    UpdateRoutes = require('./Routes/Update');
-
-const port = 8082;
+    UpdateRoutes = require('./Routes/Update'),
+    config = require('./Configuration');
 
 app.use(bodyParser.urlencoded({
     extended : false
@@ -15,5 +14,5 @@ app.use('/retrieve', RetrieveRoutes);
 app.use('/update', UpdateRoutes);
 
 app.listen(port, () => {
-    console.log('Listen up kids, the Iron Islands fleet consists of ' + port + ' ships!');
+    console.log('Listen up kids, the Iron Islands fleet consists of ' + configport + ' ships!');
 });
